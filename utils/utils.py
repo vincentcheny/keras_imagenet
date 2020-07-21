ab27cbec8f073ce7f9dd088b0df2cffbc2da14b0
vincentcheny <cy0906@163.com>
2020-07-21 21:56:08 +0800

    update search space

--- utils/utils.py
+++ utils/utils.py
@@ -13,8 +13,6 @@ def fix_randomness():
     os.environ['PYTHONHASHSEED']=str(seed_value)
     random.seed(seed_value)
     np.random.seed(seed_value)
-    print(np.random.rand())
-    np.random.seed(seed_value)
     tf.compat.v1.set_random_seed(seed_value)
 
 
